bca1803fae3ce2f0730fa8c3b4ce6a46d3eb126b
Pepela John <pepelajahy@gmail.com>
2025-05-22 00:05:24 +0300

    broken images

--- data/project-section/v1/home-page/index.ts
+++ data/project-section/v1/home-page/index.ts
@@ -33,7 +33,7 @@ export const projectSectionData: ProjectSectionProps = {
       href: 'https://milanodirouge.com/',
       slug: '/project/business-rebrand',
       image: {
-        src: '/assets/images/project/luxury.jpg ',
+        src: '/assets/images/project/lux.jpg ',
         alt: 'luxury-fashion-brand',
       },
       title: 'Luxury Design Brand',
