3d3006f6e6d722fedf64695b023c2715bbdbe7c3
Pepela John <pepelajahy@gmail.com>
2026-01-06 14:07:05 +0300

    refactor: clean up layout component formatting and update GetReferrerPage functionality

--- app/referrer.tsx
+++ app/referrer.tsx
@@ -1,26 +1,23 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
+// import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function GetReferrerPage() {
-    
-    useEffect(() => {
-        if (typeof window !== undefined) {
-          const urlParams = new URLSearchParams(window.location.search);
-          const utmSource = urlParams.get('utm_source');
-          const utmMedium = urlParams.get('utm_medium');
-          const utmCampaign = urlParams.get('utm_campaign');
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const urlParams = new URLSearchParams(window.location.search);
+      const utmSource = urlParams.get('utm_source');
+      const utmMedium = urlParams.get('utm_medium');
+      const utmCampaign = urlParams.get('utm_campaign');
 
-          console.log('UTM Source:', utmSource);
-          console.log('UTM Medium:', utmMedium);
-          console.log('UTM Campaign:', utmCampaign);
+      console.log('UTM Source:', utmSource);
+      console.log('UTM Medium:', utmMedium);
+      console.log('UTM Campaign:', utmCampaign);
 
-          // You can send this to analytics or log it
-        }
+      // You can send this to analytics or log it
+    }
   }, []);
 
-  return (
-   <></>
-  );
+  return <></>;
 }
